a6aece06b574848b5651b2c3ab804e84f1d48ba2
Paul Giroux <paul.giroux87@gmail.com>
2022-02-02 16:45:11 +0100

    Update sql_init.sql
    add importation table

--- sql_init.sql
+++ sql_init.sql
@@ -7,6 +7,7 @@ create table `elleve`(
     `nom` varchar(50) not null,
     `prenom` varchar(50) not null,
     `classe` varchar(50),
+    `id_importation` int,
     primary key(`id`)
 )ENGINE=InnoDB;
 
@@ -14,6 +15,7 @@ create table `proph`(
     `id` int AUTO_INCREMENT,
     `nom` varchar(50) not null,
     `prenom` varchar(50) not null,
+    `id_importation` int,
     primary key(`id`)
 )ENGINE=InnoDB;
 
@@ -26,10 +28,16 @@ create table `rdv`(
     `id_elleve` int not null,
     `id_proph` int not null,
     `lieu` varchar(50) not null,
+    `id_importation` int,
     CONSTRAINT uc_rdv unique(`date`, `id_elleve`),
     primary key(`id`),
     foreign key(`id_elleve`) references elleve(`id`),
     foreign key(`id_proph`) references proph(`id`)
+)ENGINE=InnoDB;
+
+create table `importation`(
+    `id` int AUTO_INCREMENT,
+    `date` timestamp not null,
 )ENGINE=InnoDB;
 
 SHOW TABLE STATUS;
